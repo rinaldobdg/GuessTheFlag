b89065fb71ef037987c171f4502ba283a1ca0e14
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-10-26 18:53:32 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Benny Rinaldo on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
